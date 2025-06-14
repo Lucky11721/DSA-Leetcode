253859a7e77794f811f08a700ccfdd0518296e38
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-06-15 00:25:49 +0530

    Time: 23 ms (87.80%) | Memory: 43.4 MB (65.40%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int triangleNumber(int[] arr) {
+         int res = 0;
+        Arrays.sort(arr);
+
+  
+        for (int i = 2; i < arr.length; ++i) {
+          
+            int left = 0, right = i - 1;
+
+            while (left < right) {
+                if (arr[left] + arr[right] > arr[i]) {
+                
+                    res += right - left;
+
+                  
+                    right--;
+                } else {
+                   
+                    left++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
