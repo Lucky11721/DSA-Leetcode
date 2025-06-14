81a26f051e591d03f39691559c50a4934ebdb7ed
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-06-14 22:51:12 +0530

    Time: 3 ms (45.81%) | Memory: 42.7 MB (22.64%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,36 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int n = nums.size();
-        int[] arr = new int[n];
-        
-        // Convert list to array
-        for (int i = 0; i < n; i++) {
-            arr[i] = nums.get(i);
-        }
-
-        Arrays.sort(arr); // Sort the array
+        int i = 0;
+        int j = nums.size() -1;
         int count = 0;
-
-        // For each element, binary search the highest j such that arr[i] + arr[j] < target
-        for (int i = 0; i < n - 1; i++) {
-            int low = i + 1, high = n - 1;
-            int res = i;
-
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (arr[i] + arr[mid] < target) {
-                    res = mid; // mid is a valid pair
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+        Collections.sort(nums);
+        while(i <= j){
+            if(nums.get(i) + nums.get(j) < target){
+                count += j-i;
+                i++;
             }
-
-            count += (res - i); // number of valid j's for current i
+            else{
+                j--;
+            }
         }
-
         return count;
+
     }
 }
