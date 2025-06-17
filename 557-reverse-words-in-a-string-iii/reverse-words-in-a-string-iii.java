61858f145d07d5f81500488d91db599df5b3c4d0
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-06-18 00:59:03 +0530

    Time: 4 ms (86.67%) | Memory: 45.3 MB (60.01%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,32 +1,13 @@
 class Solution {
     public String reverseWords(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
-        int j = 0;
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            sb.append(ch);
-
-            if (ch == ' ') {
-                reverse(sb, j, sb.length() - 2); // reverse the word before space
-                j = sb.length(); // next word starts after the space
-            }
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        
+        for (String word : words) {
+            StringBuilder rev = new StringBuilder(word);
+            result.append(rev.reverse().toString()).append(" ");
         }
-
-        // Reverse the last word
-        reverse(sb, j, sb.length() - 1);
-
-        return sb.toString();
-    }
-
-    public void reverse(StringBuilder str, int i, int j) {
-        while (i < j) {
-            char temp = str.charAt(i);
-            str.setCharAt(i, str.charAt(j));
-            str.setCharAt(j, temp);
-            i++;
-            j--;
-        }
+        
+        return result.toString().trim(); // remove the last space
     }
 }
