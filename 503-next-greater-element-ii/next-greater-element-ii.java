3c40eff96c3314650b49168e24c5ba4ca12f5e4f
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-05 23:58:58 +0530

    Time: 15 ms (70.29%) | Memory: 46.2 MB (61.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-
         int[] ans = new int[n];
-
-        Arrays.fill(ans , -1);
-
-        for(int i = 0 ; i < n  ;i ++){
-
-            boolean found  = false;
-
-            for(int j = i+1 ; j < n ; j++){
-                if(nums[j] > nums[i]){
-                    ans[i] = nums[j];
-                    found = true;
-                    break;
-                }
-            }
-
-            if(found == false){
-                for(int j = 0 ; j < i ; j++){
-                    if(nums[j] > nums[i]){
-                        ans[i] = nums[j];
-                        break;
-                    }
-                }
-            }
-        }
-        return ans;
+        Arrays.fill(ans, -1);
+        Stack<Integer> st = new Stack<>();
         
+        // Traverse the array twice (simulate circular array)
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int curr = nums[i % n];
+            
+            // Maintain a decreasing stack
+            while (!st.isEmpty() && nums[st.peek()] <= curr) {
+                st.pop();
+            }
+
+            if (i < n) { // Only fill answers in the first pass
+                if (!st.isEmpty()) {
+                    ans[i] = nums[st.peek()];
+                }
+            }
+            st.push(i % n);
+        }
+        
+        return ans;
     }
 }
