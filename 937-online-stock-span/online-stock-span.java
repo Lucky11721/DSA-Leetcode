12b89fe30e9d1cb6c3223625e68c90bd1e651cb8
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-12 21:21:13 +0530

    Time: 20 ms (99.29%) | Memory: 54 MB (91.27%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,27 +1,25 @@
 class StockSpanner {
     ArrayList<Integer> prices;
-
+    ArrayList<Integer> spans;
 
     public StockSpanner() {
         prices = new ArrayList<>();
- 
+        spans = new ArrayList<>();
     }
 
     public int next(int price) {
-         int count = 1;
+        int span = 1;
+        int i = prices.size() - 1;
 
-        for(int  i = prices.size() -1; i >= 0 ; i--){
-            if(price >= prices.get(i)){
-                count++;
-            }
-            else{
-                break;
-            }
-
+        // Traverse backwards while prices[i] <= current price
+        while (i >= 0 && prices.get(i) <= price) {
+            span += spans.get(i);
+            i -= spans.get(i);
         }
-        prices.add(price);
-     
 
-        return count;
+        prices.add(price);
+        spans.add(span);
+
+        return span;
     }
 }
