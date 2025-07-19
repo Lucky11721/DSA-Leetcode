9fa98ccddca3a676114670b309c4fa663ddd1228
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-20 01:41:37 +0530

    Time: 1 ms (99.66%) | Memory: 58.5 MB (11.84%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,31 +1,35 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] arr) {
-        int n = arr.length;
-        int minSize = Integer.MAX_VALUE;
-        int i = 0;
+    public int minSubArrayLen(int x, int[] arr) {
+      int n = arr.length;
+        
+        int i = 0; 
         int j = 0;
-        int sum = 0;
-
-        // First window expansion until sum >= target
-        while (j < n && sum < target) {
+        int sum  = 0;
+        int minSize = Integer.MAX_VALUE;
+        
+        while(j < n && sum < x){
             sum += arr[j];
             j++;
         }
-
-        // Main loop: sliding the window
-        while (i < n) {
-            if (sum >= target) {
-                minSize = Math.min(minSize, j - i); // (j - i) is length
-                sum -= arr[i];
-                i++;
-            } else if (j < n) {
-                sum += arr[j];
-                j++;
-            } else {
-                break;
+        
+        while(i < n){
+                if(sum >= x){
+                    minSize = Math.min(minSize , j-i);
+                    sum = sum - arr[i];
+                    i++;
+                }
+                else if(j < n){
+                    sum += arr[j];
+                    j++;
+                }
+                else{
+                    break;
+                }
             }
-        }
-
-        return (minSize == Integer.MAX_VALUE) ? 0 : minSize;
+        
+        
+        if(minSize == Integer.MAX_VALUE) return 0;
+        
+        return minSize;
     }
 }
