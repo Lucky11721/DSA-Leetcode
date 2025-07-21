5ed8d8a006dbfba9d7d807a059241492d116264d
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-22 00:14:09 +0530

    Time: 2 ms (99.81%) | Memory: 47.5 MB (75.15%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,35 +1,46 @@
 class Solution {
-    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int n = grumpy.length;
-       int ans= 0;
-        for(int  a =  0 ; a <  n ; a++){
-            if(grumpy[a] == 0){
-                ans += customers[a];
+    public int maxSatisfied(int[] arr, int[] grumpy, int minutes) {
+        int n = arr.length;
+
+        int maxunsat = 0;
+        
+        int i = 0;
+        int j = minutes - 1;
+        int curr_unsat = 0;
+        for (int x = 0; x <= j; x++) {
+            if (grumpy[x] == 1) {
+                curr_unsat += arr[x];
             }
         }
-
-        int i  = 0 ;
-        int j = minutes -1;
-    
-       int maxunsat = 0;
-       int unsat = 0;
-        while( j < n){
-           
-            for(int a = i ; a <= j ; a++){
-                if(a == n) break;
-                if(grumpy[a] == 1 ){
-                   unsat += customers[a];
-                }
-
-              
+        int a = 0;
+        int b = 0;
+        while (j < n) {
+            if (curr_unsat > maxunsat) {
+                maxunsat = curr_unsat;
+                a = i;
+                b = j;
             }
-              maxunsat = Math.max(unsat , maxunsat);
 
-                i++;
-                j++;
-                 unsat = 0;
+            i++;
+            j++;
+
+            if (j < n && grumpy[j] == 1)
+                curr_unsat += arr[j];
+            if (grumpy[i - 1] == 1)
+                curr_unsat -= arr[i - 1];
         }
 
-        return ans + maxunsat;
+        for (int x = a; x <= b; x++) {
+            grumpy[x] = 0;
+        }
+
+        int ans = 0;
+
+        for (int x = 0; x < n; x++) {
+            if (grumpy[x] == 0) {
+                ans += arr[x];
+            }
+        }
+        return ans;
     }
 }
