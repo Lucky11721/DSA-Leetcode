9bbd9ef7116b48ad81b802b40600ddd69ff47497
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-27 19:46:03 +0530

    Time: 2 ms (80.10%) | Memory: 46.1 MB (70.71%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,21 +1,20 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int n = arr.length;
-        int noz = 0;
-        for(int ele : arr){
-            if(ele == 0){
-             noz++;
-            }
-        }
+         int n = arr.length;
+         int[] ans = new int[n];
+      int index = 0;
+         for(int i = 0 ; i <  n ; i++){
 
-        for(int x = 0 ; x <  noz ; x++){
-            for(int i = 0 ; i <n-1-x ; i++){
-              if(arr[i]==0 ){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-              }
-        }
+            if(arr[i] != 0){
+                ans[index++] = arr[i];
+            }
+         }
+         for(int i = index ; i < n ; i++){
+         ans[i] = 0;  
+         }
+
+         for(int i =0 ;i <  n ; i++){
+            arr[i] = ans[i];
+         }
     }
-}
 }
