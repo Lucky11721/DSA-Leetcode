e5f2c3a3924c6f1558a7c6bccf0d8f31be0f3dd2
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-07-29 00:53:34 +0530

    Time: 2 ms (42.20%) | Memory: 57.9 MB (21.49%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,21 +1,38 @@
 class Solution {
     public int firstMissingPositive(int[] arr) {
-
         int n = arr.length;
-        HashSet<Integer> set = new HashSet<>();
-        for (int ele : arr) {
-            set.add(ele);
-        }
-
-        int smallest = 1;
-        int i = 0;
-        while (i < n) {
-            if (set.contains(smallest)) {
-                smallest++;
+        // first loop make negative value 0
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+               arr[i] = 0;
             }
-            i++;
         }
-        return smallest;
+        // 2 loop start marking visited element or marked present in array by making the value of array -1 index  = -ve
 
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(arr[i]);
+            if( val >= 1 && val <= n){
+                int index = val -1;
+                if(arr[index] > 0){
+                    arr[index] = -1 *arr[index];
+                }
+                else if(arr[index] == 0){
+                    arr[index] = -1*(n+1);
+                }
+            }
+
+            
+                
+        }
+
+       
+
+        for (int i = 1; i <=n; i++) {
+            int index = i - 1;
+            if(arr[index] >= 0 ) return i;
+            
+        }
+
+        return n +1 ;
     }
 }
