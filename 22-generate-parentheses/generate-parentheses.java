e04360a34235e68cd52825ff5f5952086d820469
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-02 01:39:36 +0530

    Time: 12 ms (5.12%) | Memory: 45.4 MB (5.95%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,18 +1,41 @@
 class Solution {
     List<String> ans;
+
     public List<String> generateParenthesis(int n) {
         ans = new ArrayList<>();
-        helper(0 , 0, n , "");
+
+        helper("", 2 * n);
+
         return ans;
     }
 
-    public void helper(int open , int close , int n , String str){
-        if(str.length() == 2* n){
-            ans.add(str);
+    public void helper(String str, int len) {
+        if (str.length() == len) {
+
+            if (isValid(str) == true) {
+                ans.add(str);
+            }
             return;
         }
 
-        if(open < n) helper(open+1 , close , n , str + "(");
-        if(close <  open) helper(open , close+1 , n , str + ")");
+        helper(str + "(", len);
+        helper(str + ")" , len);
+    }
+
+    public boolean isValid(String str) {
+        int count = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(')
+                count++;
+            else
+                count--;
+        
+        if(count < 0) return false;
+        }
+        if (count == 0)
+            return true;
+
+        return false;
     }
 }
