7a666a843d0e8fc31d98566bb56426ebbb55df77
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-06 23:41:42 +0530

    Time: 1 ms (84.37%) | Memory: 40.8 MB (69.34%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-    return (int)Math.sqrt(x);
+        if (x == 0) return 0;
+        int low = 1;
+        int high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) {
+                return mid;
+            } else if (mid > x / mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return  -1;
     }
 }
