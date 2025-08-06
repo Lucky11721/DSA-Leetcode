a25ca5c955c19c9fc5f9153243bce11e174aacfb
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-07 01:27:20 +0530

    Time: 27 ms (17.70%) | Memory: 45.2 MB (59.78%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] arr, int h) {
+
+        int n = arr.length;
+        int max_value = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max_value) {
+                max_value = arr[i];
+            }
+        }
+
+        int left = 1;
+        int right = max_value;
+
+        int ans = right;
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            int curr_hour = 0;
+            for (int i = 0; i < n; i++) {
+                   curr_hour += Math.ceil((double) arr[i] / mid);
+            }
+            if (curr_hour <= h) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
