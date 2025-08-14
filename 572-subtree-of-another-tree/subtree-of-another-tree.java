35c34585b848ae6bcbd81c51790904df249482de
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-14 12:13:42 +0530

    Time: 2 ms (98.11%) | Memory: 44.4 MB (52.56%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,24 +15,19 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
-        }
-
-        if (root.val == subRoot.val && compare(root, subRoot)) {
+        if(root == null || subRoot == null) return false;
+        if(root.val == subRoot.val && isSame(root , subRoot)){
             return true;
         }
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
+        return isSubtree(root.left , subRoot) ||  isSubtree(root.right , subRoot);
+        
     }
+    public boolean isSame(TreeNode a , TreeNode b){
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+        if(a.val != b.val) return false;
 
-    public boolean compare(TreeNode root, TreeNode subRoot) {
-        if (root == null && subRoot == null) {
-            return true;
-        }
-        if (root == null || subRoot == null || root.val != subRoot.val) {
-            return false;
-        }
-        return compare(root.left, subRoot.left) && compare(root.right, subRoot.right);
+        return isSame(a.left , b.left) && isSame(a.right,b.right);
     }
 }
