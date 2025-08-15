1fba9c0b70bfc0242326ec73e9d1f0746fb5747c
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-15 21:49:15 +0530

    Time: 3 ms (33.97%) | Memory: 44.1 MB (87.25%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,25 +14,29 @@
  * }
  */
 class Solution {
+  
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = preorder.length;
-        return construct(preorder , inorder , 0 , n-1 , 0 , n-1);
+        
+        return helper(preorder, inorder , 0 , n-1, 0 , n-1);
+
     }
-    public TreeNode construct(int[] pre , int [] in , int prelow , int prehigh , int inlow , 
-    int inhigh ){
-        if(prelow > prehigh || inlow > inhigh){
-            return null;
+    public TreeNode helper(int[] pre , int[] in , int prelow , int prehigh , int inlow , int inhigh){
+        if(prelow > prehigh || inlow > inhigh) return null;
+
+         TreeNode root = new TreeNode(pre[prelow]);
+        int root_index = 0;
+        for(int  i =0 ; i < in.length ; i++){
+            if(root.val == in[i]){
+                root_index = i;
+                break;
+            }
         }
-     TreeNode root = new TreeNode(pre[prelow]);
-     int root_index = 0;
-     for(int i = 0 ; i < in.length ; i++){
-        if(in[i] == root.val){
-            root_index  = i;
-        }
-     }
         int lst_size = root_index - inlow;
-       root.left = construct(pre  , in , prelow + 1 ,prelow + lst_size  , inlow , root_index -1);
-        root.right = construct(pre , in ,prelow + lst_size + 1, prehigh , root_index +1 , inhigh);
-     return root;
+
+        root.left = helper(pre , in , prelow+1 , prelow + lst_size , inlow , root_index-1);
+        root.right = helper(pre , in , prelow + lst_size+1 , prehigh , root_index+1 , inhigh);
+
+        return root;
     }
 }
