fad6057e92d13e8bae92233d3972dcb3b2b986d5
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-15 22:10:08 +0530

    Time: 3 ms (27.34%) | Memory: 43.9 MB (98.39%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -16,26 +16,23 @@
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int n = inorder.length;
-     return construct(inorder , postorder , 0 , n-1 , 0 , n-1);
-
+       return helper(postorder , inorder , 0 , n-1 , 0 , n-1);
     }
-    public TreeNode construct(int[] in , int[] post , int postlow , int posthigh , int inlow , int inhigh){
-        
-         if(postlow > posthigh || inlow > inhigh){
-            return null;
-         }
-         TreeNode root = new TreeNode(post[posthigh]);
-         int root_index = 0;
-         for(int i = 0 ; i < in.length ; i++){
-            if(in[i] == root.val){
-               root_index = i;
-               break;
+    public TreeNode helper(int[] post , int[] in , int postlow , int posthigh , int inlow , int inhigh){
+        if(postlow > posthigh || inlow > inhigh) return null;
+
+        TreeNode root = new TreeNode(post[posthigh]);
+        int root_index = 0;
+        for(int i = 0; i <= inhigh  ; i++){
+            if(root.val == in[i]){
+                root_index = i;
+                break;
             }
-         }
-         int lst_size = root_index - inlow;
-         root.left = construct(in , post , postlow, postlow + lst_size -1 , inlow , root_index-1 );
-         
-         root.right = construct(in , post , postlow + lst_size , posthigh -1 , root_index +1 , inhigh );
-         return root;
+        }
+        int lst_size = root_index - inlow;
+
+        root.left = helper(post , in , postlow , postlow + lst_size -1 , inlow , root_index-1);
+        root.right = helper(post , in , postlow + lst_size , posthigh -1 , root_index+1 , inhigh);
+        return root;
     }
 }
