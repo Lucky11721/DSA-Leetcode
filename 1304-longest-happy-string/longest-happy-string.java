b7e7b1d7e9596a84b87ac89105521c3ee3fc6374
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-20 15:13:43 +0530

    Time: 2 ms (54.68%) | Memory: 41 MB (61.15%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.java
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -1,41 +1,41 @@
 class Solution {
-    class Pair {
+    class Pair{
         char ch;
         int count;
-        Pair(char ch, int count) {
+        Pair(char ch , int count){
             this.ch = ch;
             this.count = count;
         }
     }
-
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<Pair> maxheap = new PriorityQueue<>((x, y) -> y.count - x.count);
+        char chara = 'a';
+        char charb = 'b';
+        char charc = 'c';
 
-        if (a > 0) maxheap.add(new Pair('a', a));
-        if (b > 0) maxheap.add(new Pair('b', b));
-        if (c > 0) maxheap.add(new Pair('c', c));
+        PriorityQueue<Pair> maxheap = new PriorityQueue<>((x,y) -> Integer.compare(y.count , x.count));
+       if(a > 0) maxheap.add(new Pair(chara , a));
+        if(b > 0)maxheap.add(new Pair(charb , b));
+       if(c > 0) maxheap.add(new Pair(charc , c));
 
         StringBuilder sb = new StringBuilder();
 
-        while (!maxheap.isEmpty()) {
-            Pair first = maxheap.poll();
-
-            int len = sb.length();
-            if (len >= 2 && sb.charAt(len - 1) == first.ch && sb.charAt(len - 2) == first.ch) {
-                // Can't use first now, try second
-                if (maxheap.isEmpty()) break;
-                Pair second = maxheap.poll();
-                sb.append(second.ch);
-                second.count--;
-                if (second.count > 0) maxheap.add(second);
-                maxheap.add(first); // put first back
-            } else {
-                sb.append(first.ch);
-                first.count--;
-                if (first.count > 0) maxheap.add(first);
-            }
+        while(!maxheap.isEmpty()){
+        Pair p1 = maxheap.poll();
+        int n = sb.length();
+        if(sb.length() >= 2 && sb.charAt(n-1) == p1.ch && sb.charAt(n-2) == p1.ch){
+            if(maxheap.isEmpty()) break;
+            Pair p2 = maxheap.poll();
+            sb.append(p2.ch);
+            p2.count  = p2.count -1;
+            if(p2.count > 0) maxheap.add(p2);
+            if(p1.count > 0) maxheap.add(p1);
         }
-
+        else{
+            sb.append(p1.ch);
+            p1.count = p1.count-1;
+            if(p1.count > 0) maxheap.add(p1);
+        }
+        }
         return sb.toString();
     }
 }
