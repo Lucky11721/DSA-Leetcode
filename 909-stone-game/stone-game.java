9ae7789146d9a2d4cc46d327654e78065e7c10f3
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-25 16:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (37.05%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,22 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int[] dp = new int[piles.length +1 ];
-        Arrays.fill(dp , -1);
-        int alice = helper(piles , 0 , dp);
-        int bob = helper(piles , 1 ,dp);
-    
-        if(alice > bob)return true;
-
-        return false;
-    }
-    public int helper(int[] arr , int index , int[] dp){
-        if(index >= arr.length) return 0;
-      if(dp[index] != -1) return dp[index];
-
-        int pick = arr[index] + helper(arr , index+1 , dp);
-        int skip = helper(arr , index+1 , dp);
-
-
-        return dp[index] =  Math.max(pick , skip);
+        return true;
     }
 }
