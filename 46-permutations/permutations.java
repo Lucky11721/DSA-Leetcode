3b3c5c1ecd586138978a9e22cc94ab9a97ea18f4
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-08-29 02:21:24 +0530

    Time: 1 ms (92.67%) | Memory: 44.4 MB (82.09%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,29 @@
-class Solution {List<List<Integer>> ans;
+class Solution {
+    List<List<Integer>> ans;
     public List<List<Integer>> permute(int[] nums) {
         ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        helper(nums , list);
+        helper(nums , list , 0);
         return ans;
     }
 
-    public void helper(int[] arr , List<Integer> list){
+    public void helper(int[] arr , List<Integer> list , int index){
         if(list.size() == arr.length){
             ans.add(new ArrayList<>(list));
             return;
         }
 
-        for(int i = 0 ; i < arr.length ; i++){
-                    if(list.contains(arr[i]) == false){
-                        list.add(arr[i]);  // Do something kuch kara
-                    helper(arr,list); // explore kara
-                    list.remove(list.size() - 1); // fir undo kr diya 
-                    }
-        }
+        for(int i = index ; i < arr.length ; i++){
+                swap(arr, i , index);
+                list.add(arr[index]);
+                helper(arr , list , index +1 );
+                swap(arr , i , index);
+               list.remove(list.size() -1);
+                    }    
+    }
+    public void swap( int[] arr,int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
