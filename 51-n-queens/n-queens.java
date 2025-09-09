ea97fd9a7b1750e8d7ddedc0450c869f4afe57e1
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-09-10 03:41:34 +0530

    Time: 12 ms (5.01%) | Memory: 45.3 MB (15.05%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,70 @@
+
+
+class Solution {
+    List<List<String>> final_ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] ans = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = '.';
+            }
+        }
+
+        System.out.println(Arrays.deepToString(ans));  // Fix: use deepToString to print 2D array
+        helper(ans, 0, n);
+        return final_ans;
+    }
+
+    public void helper(char[][] ans, int row, int n) {
+        if (row >= n) {
+            String[] str = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                str[i] = "";
+                for (int j = 0; j < ans[0].length; j++) {
+                    str[i] += ans[i][j];
+                }
+            }
+            List<String> temp = Arrays.asList(str);  // Fix: better way to convert array to List
+            final_ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(ans, row, col)) {
+                ans[row][col] = 'Q';
+                helper(ans, row + 1, n);
+                ans[row][col] = '.';
+            }
+        }
+    }
+
+    public boolean isValid(char[][] ans, int row, int col) {
+        int n = ans.length;
+
+        // Upward column
+        for (int i = row - 1; i >= 0; i--) {
+            if (ans[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (ans[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (ans[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
