8979aab033380ca4d819f35fc0a10ad84d8a6780
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-09-18 03:28:54 +0530

    Time: 622 ms (5.70%) | Memory: 8.2 MB (81.44%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        return helper(s, p, 0, 0, n, m);
+    }
+
+    bool helper(const string &s, const string &p, int i, int j, int n, int m) {
+        if (i == n && j == m) return true;
+        if (j == m) return false;
+
+        bool first = false;
+        if (i < n && j < m && (s[i] == p[j] || p[j] == '.')) {
+            first = true;
+        }
+
+        bool take = false;
+        bool skip = false;
+        bool normal = false;
+
+        if (j + 1 < m && p[j + 1] == '*') {
+            skip = helper(s, p, i, j + 2, n, m);
+            if (i < n && j < m && (s[i] == p[j] || p[j] == '.')) {
+                take = first && helper(s, p, i + 1, j, n, m);
+            }
+        } else {
+            normal = first && helper(s, p, i + 1, j + 1, n, m);
+        }
+
+        return normal || take || skip;
+    }
+};
