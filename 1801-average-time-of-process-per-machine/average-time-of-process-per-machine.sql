452329c00d398714d85cb98b62e6a84f4c7458c9
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-09-24 23:16:32 +0530

    Time: 269 ms (68.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+
+
+select a.machine_id , round(avg((e.timestamp - a.timestamp))  , 3)as processing_time 
+from Activity a
+join Activity e
+on a.machine_id = e.machine_id
+and a.process_id = e.process_id 
+and a.activity_type  = 'start'
+and e.activity_type = 'end'
+group by a.machine_id;
