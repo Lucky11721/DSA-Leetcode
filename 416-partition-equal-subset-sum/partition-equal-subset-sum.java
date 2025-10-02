51c465c75461428fe5aff3ea417a0deaa511c042
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-02 23:12:03 +0530

    Time: 95 ms (20.18%) | Memory: 57.3 MB (32.57%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,26 +1,25 @@
 class Solution {
-    Boolean dp[][];
+    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
-        int n =nums.length;
         int sum = 0;
         for(int ele : nums){
             sum += ele;
         }
- dp = new Boolean[n+1][sum/2 + 1];
         if(sum % 2 != 0) return false;
-        else return helper(nums ,sum /2 , 0);
+        dp = new Boolean[nums.length +1 ][sum/2 +1];
+
+        return helper(nums , 0 , sum /2);
     }
-    public boolean helper(int[] arr , int target , int index){
+
+    public boolean helper(int[] arr , int index , int target){
         if(target < 0) return false;
-        if(index == arr.length){
+        if(index == arr.length ){
             if(target == 0) return true;
             else return false;
         }
         if(dp[index][target] != null) return dp[index][target];
-
-        boolean pick = helper(arr , target - arr[index] , index +1);
-
-        boolean skip = helper(arr , target , index+1);
+        boolean pick = helper(arr , index+1 , target - arr[index]);
+        boolean skip = helper(arr , index +1 , target);
 
         return dp[index][target] = pick || skip;
     }
