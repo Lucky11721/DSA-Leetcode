8c736fcb01960911deded10a16125dacef260f35
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-02 23:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (61.17%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,9 +1,11 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean visited[] = new boolean[n+1];
-      int ans = 0;
-        for(int i = 0 ; i < n ; i++){
+        boolean[] visited = new boolean[n+1];
+
+        int ans = 0;
+
+        for(int i = 0 ; i  < n ; i++){
             if(visited[i] == false){
                 dfs(i , visited , isConnected);
                 ans++;
@@ -11,33 +13,15 @@ class Solution {
         }
         return ans;
     }
+
     public void dfs(int index , boolean[] visited , int[][] graph){
 
         visited[index] = true;
 
-        for(int col = 0 ; col < graph[index].length ; col++){
+        for(int col = 0; col < graph[index].length ; col++){
             if(visited[col] == false && graph[index][col] == 1){
                 dfs(col , visited , graph);
             }
         }
     }
-
-
-    /*public void helper(boolean[] visited , int[][] graph , int index){
-
-        Queue<Integer> que = new LinkedList<>();
-        que.add(index);
-        visited[index] = true;
-
-        while(que.isEmpty() == false){
-            int ele = que.remove();
-
-            for(int i = 0 ; i < graph.length ; i++  ){
-                if(visited[i] == false && graph[ele][i] == 1){
-                    que.add(i);
-                    visited[i] = true;
-                }
-            }
-        }
-    }*/
 }
