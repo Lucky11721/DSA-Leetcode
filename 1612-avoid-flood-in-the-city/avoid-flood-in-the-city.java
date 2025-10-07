fbfdbb8e578d9136f931e675892609114ba61d4a
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-07 16:38:23 +0530

    Time: 64 ms (88.13%) | Memory: 62.4 MB (36.99%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -2,30 +2,22 @@ class Solution {
     public int[] avoidFlood(int[] arr) {
         int n = arr.length;   
          HashMap<Integer ,Integer> map = new HashMap<>();
-         List<Integer> list = new ArrayList<>();
+         TreeSet<Integer> set = new TreeSet<>();
          int ans[] = new int[n];
          for(int i = 0 ; i < n ; i++){
             if(arr[i] == 0){
-                list.add(i);
+                set.add(i);
                 ans[i] = 1;
             }
             else{
                 ans[i] = -1;
                 if(map.containsKey(arr[i])){
                     int flood_index = map.get(arr[i]);
-                    int dry = -1;
-
-                    for(int j = 0 ; j< list.size() ; j++){
-                        if(list.get(j) > flood_index){
-                            dry = j;
-                            break;
-                        }
-                    }
-
-                    if(dry == -1) return new int[0];
-                    int dryday = list.get(dry);
-                    ans[dryday] = arr[i];
-                    list.remove(dry);
+                    Integer dry = set.higher(flood_index);
+                    if(dry == null) return new int[0];
+        
+                    ans[dry] = arr[i];
+                    set.remove(dry);
                 }
                 map.put(arr[i] , i);
             }
