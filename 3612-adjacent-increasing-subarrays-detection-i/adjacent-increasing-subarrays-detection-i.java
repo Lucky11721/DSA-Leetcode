aeee6c91e1fe9635efb22e52e06643fa1e2fa4d0
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-14 22:57:08 +0530

    Time: 3 ms (20.59%) | Memory: 44.7 MB (38.24%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -1,44 +1,42 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
         int n = nums.size();
-        if (k == 1) return true;
-
-        // Special case: exactly 2*k elements
-        if (n == 2 * k) {
-            return helper(nums, 0, k, k, 2 * k, k);
+        if(k == 1) return true;
+        if(n == 2*k){
+           return helper(nums, 0, k, k, 2 * k, k);
         }
-
-        // General case: sliding over possible positions
-        for (int i = 0; i + 2 * k <= n; i++) {
-            if (helper(nums, i, i + k, i + k, i + 2 * k, k)) {
+        for(int i = 0 ; i +2* k <=n ; i++){
+            if(helper(nums , i , i+k , i+k , i + 2* k , k)){
                 return true;
             }
         }
-        return false;
+return false;
+
     }
 
-    private boolean helper(List<Integer> list, int i, int j, int ik, int jk, int k) {
-        int count = 0;
-
-        // first subarray [i, j)
-        for (int x = i + 1; x < j; x++) {
-            if (list.get(x) > list.get(x - 1)) {
+    public boolean helper(List<Integer> list , int i , int j , int ik , int jk , int k ){
+        int count =0 ;
+        for(int x = i +1 ; x < j ; x++){
+            if(list.get(x) > list.get(x-1)){
                 count++;
-            } else {
-                return false; // not strictly increasing
+            }
+            else{
+                return false;
             }
         }
 
-        // second subarray [ik, jk)
-        for (int x = ik + 1; x < jk; x++) {
-            if (list.get(x) > list.get(x - 1)) {
+        for(int x = ik +1 ; x < jk ; x++){
+            if(list.get(x) > list.get(x-1)){
                 count++;
-            } else {
-                return false; // not strictly increasing
+            }
+            else{
+                return false;
             }
         }
+        System.out.println(count);
+      
 
-        // both must be strictly increasing
-        return count == 2 * (k - 1);
+      return count == 2*(k-1);
+
     }
 }
