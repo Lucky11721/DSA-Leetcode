e6b45375115e98089bff8de7272f74a49346b541
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-15 22:55:55 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (12.50%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -1,34 +1,29 @@
 class Solution {
-    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        int n = nums.size();
-        if(k == 1) return true;
-        if(n == 2*k){
-           return helper(nums, 0, k, k, 2 * k, k);
-        }
-        for(int i = 0 ; i +2* k <=n ; i++){
-            if(helper(nums , i , i+k , i+k , i + 2* k , k)){
-                return true;
+    public boolean hasIncreasingSubarrays(List<Integer> arr, int k) {
+        int n  = arr.size();
+        int prev_length = 0;
+        int curr_length  =1;
+        for(int i = 1 ; i <  n ; i++){
+            if(arr.get(i) > arr.get(i-1)){
+                curr_length++;
+                if(curr_length/2 >= k){
+                    return true;
+                }
+            }
+            else{
+                if(prev_length >= k && curr_length >= k){
+                    return true;
+                }
+                else{
+                    prev_length = curr_length;
+                    curr_length = 1;
+                }
             }
         }
-return false;
 
-    }
-
-    public boolean helper(List<Integer> list , int i , int j , int ik , int jk , int k ){
-       
-       for(int x = i+1 ; x < j ; x++){
-        if(list.get(x) <= list.get(x-1)){
-            return false;
-        }
-       }
-
-       for(int x = ik+1 ; x < jk ; x++){
-         if(list.get(x) <= list.get(x-1)){
-            return false;
-        }
-       }
-
-       return true;
-
+      if(prev_length >= k && curr_length >= k){
+                    return true;
+                }
+        return false;
     }
 }
