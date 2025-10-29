2d9981b2877cc8fff0169d8cf1ab0909d142a9e1
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-10-30 00:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (9.69%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,26 +1,17 @@
 class Solution {
     public int smallestNumber(int n) {
-        int size = 0;
-        int temp  = n;
-        while(temp > 0){
-            size++;
-            temp = temp>>1;
-        }
-        System.out.println(size);
-        int[] set_bit = new int[size];
-        int num = size-1;
-        for(int i = num ; i >= 0 ; i--){
-            set_bit[i] = 1;
-        }
-         System.out.println(Arrays.toString(set_bit));
 
-        int ans = 0;
-        int power = 0;
-          for(int i = num ; i >= 0 ; i--){
-            ans += Math.pow(2 , power) *set_bit[i];
-            power++;
+        while(isSet(n) == false){
+           n++; 
         }
 
-        return ans;
+        return n;
+    }
+
+    public boolean isSet(int num){
+        int check = num & (num+1);
+
+
+        return check == 0;
     }
 }
