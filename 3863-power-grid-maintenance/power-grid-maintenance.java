819f8f32b1119b014fbc6aa089c3c8aefde38895
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-11-07 00:28:23 +0530

    Time: 290 ms (6.28%) | Memory: 288.2 MB (5.02%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.java
+++ 3863-power-grid-maintenance/power-grid-maintenance.java
@@ -1,77 +1,73 @@
 class Solution {
-
-    Map<Integer, TreeSet<Integer>> componentStations = new HashMap<>();
-
-    private void dfs(int node,
-                     Map<Integer, List<Integer>> adj,
-                     int[] visited,
-                     int componentId,
-                     int[] componentOf) {
-
-        visited[node] = 1;
-        componentOf[node] = componentId;
-
-        componentStations.putIfAbsent(componentId, new TreeSet<>());
-        componentStations.get(componentId).add(node);
-
-        for (int neighbor : adj.getOrDefault(node, new ArrayList<>())) {
-            if (visited[neighbor] == 0) {
-                dfs(neighbor, adj, visited, componentId, componentOf);
-            }
-        }
-    }
-
+     int[] component;
+    HashMap<Integer ,TreeSet<Integer>> map = new HashMap<>();
     public int[] processQueries(int c, int[][] connections, int[][] queries) {
-
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-
-        // Build adjacency list
-        for (int[] edge : connections) {
+        List<List<Integer>> list = new ArrayList<>();
+        component = new int[c+1];
+        for(int i  = 0 ; i  < c +1 ; i++){
+            list.add(new ArrayList<>());
+        }
+        for(int[] edge :connections ){
             int u = edge[0];
             int v = edge[1];
 
-            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            list.get(u).add(v);
+            list.get(v).add(u);
         }
-
-        int[] visited = new int[c + 1];
-        int[] componentOf = new int[c + 1];
-
-        // DFS to assign component ids
-        for (int node = 1; node <= c; node++) {
-            if (visited[node] == 0) {
-                dfs(node, adj, visited, node, componentOf);
+        System.out.println(Arrays.toString(component));
+        boolean[] visited = new boolean[c+1];
+        for(int i = 1 ; i <= c ; i++){
+            if(visited[i] == false){
+                dfs(list , visited ,  i , i);
             }
         }
+         System.out.println(Arrays.toString(component));
+List<Integer> ans= new ArrayList<>();
+         for(int[] query : queries){
+            int type = query[0];
+            int x = query[1];
+            int cmp = component[x];
+            if(type == 1){
 
-        List<Integer> resultList = new ArrayList<>();
-
-        // Process queries
-        for (int[] q : queries) {
-            int type = q[0];
-            int x = q[1];
-
-            int compId = componentOf[x];
-            TreeSet<Integer> set = componentStations.get(compId);
-
-            if (type == 1) {
-                if (set.contains(x)) {
-                    resultList.add(x);
-                } else if (!set.isEmpty()) {
-                    resultList.add(set.first());
-                } else {
-                    resultList.add(-1);
+                if(map.get(cmp).contains(x)){
+                    ans.add(x);
                 }
-            } else {
-                set.remove(x); //offline
+                else if(map.get(cmp).isEmpty() == false){
+                    ans.add(map.get(cmp).first());
+                }
+                else{
+                    ans.add(-1);
+                }
+            }
+            else{
+                map.get(cmp).remove(x);
+            }
+         }
+         System.out.println(ans);
+         int[] arr = new int[ans.size()];
+         for(int i = 0 ;i < arr.length ; i++){
+            arr[i] = ans.get(i);
+         }
+        return arr;
+    }
+
+    public void dfs( List<List<Integer>> list  , boolean[] visited , int cmpId , int node){
+        visited[node] = true;
+        component[node] = cmpId;
+
+        if(map.containsKey(cmpId)){
+            map.get(cmpId).add(node);
+        }
+        else{
+            TreeSet<Integer> set = new TreeSet<>();
+            set.add(cmpId);
+            map.put(cmpId , set);
+        }
+
+        for(int neigh : list.get(node)){
+            if(visited[neigh] == false){
+                dfs(list , visited , cmpId , neigh);
             }
         }
-
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = resultList.get(i);
-        }
-
-        return result;
     }
 }
