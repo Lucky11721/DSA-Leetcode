d701878235325953fb6371941f85afec5961eefe
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-11-15 00:13:43 +0530

    Time: 67 ms (39.47%) | Memory: 92.6 MB (6.14%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,24 +1,28 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] ans = new int[n][n];
-
-        int length = ans.length;
-        int colLength = ans[0].length;
+        int[][] diff = new int[n][n];
 
         for(int[] query : queries){
-            int startRow = query[0];
-            int startCol = query[1];
+            int  startRow = query[0];
             int endRow = query[2];
+            int startCol = query[1];
             int endCol = query[3];
 
-            for(int i = startRow ; i <=endRow  ; i++){
-                for(int j = startCol ; j <= endCol ; j++){
-                    ans[i][j] += 1;
+            for(int i = startRow ; i <= endRow ; i++){
+                diff[i][startCol]+=1;
+                if(endCol +1< n){
+                    diff[i][endCol+1] -=1;
                 }
+            }
+        }
+        System.out.println(Arrays.deepToString(diff));
+        for(int i = 0; i < n ; i++){
+            for(int j = 1 ; j< n ; j++ ){
+                diff[i][j] +=  diff[i][j-1];
             }
         }
 
 
-return ans;
+return diff;
     }
 }
