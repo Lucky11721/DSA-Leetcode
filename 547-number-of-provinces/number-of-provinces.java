eb71247afa6df179af02814dcfd510728094424f
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-11-18 00:07:41 +0530

    Time: 7 ms (5.23%) | Memory: 47.3 MB (71.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,34 +1,38 @@
 class Solution {
     int[] parent;
+
+    public int findLeader(int a){
+        if(parent[a]  == a) return a;
+
+        return findLeader(parent[a]);
+    }
+
+    public void union(int a , int b){
+        int leaderA = findLeader(a);
+        int leaderB = findLeader(b);
+        if(leaderA != leaderB){
+            parent[leaderA] = leaderB;
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         parent = new int[n+1];
-        for(int i = 0 ; i <  n +1 ;i++){
+        for(int i = 0 ; i < n ; i++){
             parent[i] = i;
         }
 
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j <  n ; j++){
+            for(int j = 0 ; j < n; j++){
 
-                if(i!= j && isConnected[i][j] == 1) union(i+1,j+1);
+                if(isConnected[i][j] == 1) union(i +1, j +1);
             }
         }
+
         int count = 0;
-        for(int i = 1 ; i <=n ; i++){
+        for(int i = 0 ;i < n+1 ; i++){
             if(parent[i] == i) count++;
         }
-        return count;
-    }
-    public void union(int a , int b){
-        int leaderA = find(a);
-        int leaderB = find(b);
-        if(leaderA != leaderB){
-             parent[leaderB] = leaderA;
-        }
-    }
-    public int find(int a){
-        if(parent[a] == a) return a;
 
-        return find(parent[a]);
+        return count;
     }
 }
