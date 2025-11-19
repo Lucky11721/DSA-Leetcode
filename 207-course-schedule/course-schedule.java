178c665d58b353cb57b7fcba3f9f063af5e5096e
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-11-20 00:24:18 +0530

    Time: 19 ms (6.81%) | Memory: 47.2 MB (9.94%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,45 @@
 class Solution {
-    boolean ans = true;
-    public boolean canFinish(int V, int[][] graph) {
-        List<List<Integer>> list = new ArrayList<>();
+    List<Integer> topo = new ArrayList<>();
+    public boolean canFinish(int V, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0 ; i  < V ; i++){
-            list.add(new ArrayList<>());
-        }
-        for(int i = 0 ; i < graph.length ; i++){
-            int parent = graph[i][0];
-            int child = graph[i][1];
-            list.get(parent).add(child);
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        boolean[] visited = new boolean[V];
-        boolean[] path = new boolean[V];
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prereq = edge[1];
+            adj.get(prereq).add(course);
 
-        for(int i = 0; i < list.size() ; i++){
-            if(visited[i] == false){
-                dfs(list , visited , path ,i);
+        }
+        int[] in  = new int[V];
+        for(int i = 0 ; i < V ; i++){
+            for(int node : adj.get(i)){
+                in[node] += 1;
             }
         }
-        
-        return ans;
 
-    }
+        toposort_bfs(adj , in);
+        System.out.println(Arrays.toString(in)  + "" + "size" + topo.size() + "  " + topo);
+        if(topo.size() != V) return false;
+        return true;
+    }  
 
-    public void dfs(List<List<Integer>> list , boolean[] visited , boolean[] path , int node){
-        visited[node] = true;
-        path[node] = true;
+    public void toposort_bfs(List<List<Integer>> adj , int[] in){
+        Queue<Integer> que = new LinkedList<>();
 
-        for(int child : list.get(node)){
-            if(!visited[child]){
-                dfs(list , visited , path , child);
-            }
-            else if(visited[child] == true && path[child] == true){
-                 ans = false;
-                 return;
-            }
-
+        for(int i = 0; i < in.length; i++){
+            if(in[i] == 0) que.add(i);
         }
-        path[node] = false;
-        
-    }
+
+        while(que.isEmpty()== false){
+            int vertex = que.remove();
+            topo.add(vertex);
+            for(int neigh : adj.get(vertex)){
+                in[neigh] -= 1;
+                if(in[neigh] == 0) que.add(neigh);
+            }
+        }
+}
 }
