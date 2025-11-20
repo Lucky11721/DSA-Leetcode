182e8809895035c6f38417dee573fead5b37eb3a
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-11-20 15:48:31 +0530

    Time: 1 ms (73.58%) | Memory: 44.7 MB (36.17%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,38 +1,51 @@
 class Solution {
-    int[] parent;
+    int parent[];
+    int size[];
     int[] ans;
     public int[] findRedundantConnection(int[][] edges) {
-
         int n = edges.length;
-        parent = new  int[n+1];
-        ans = new int[2];
+        parent = new int[n+1];
+        size  =new int[n+1];
         for(int i = 0 ; i < n+1 ; i++){
             parent[i] = i;
+            size[i] = 1;
         }
+        ans=  new int[2];
+
         for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            union(u , v);
+            int a =  edge[0];
+            int b = edge[1];
+
+            union(a,b);
+
         }
+
         return ans;
     }
 
     public void union(int a , int b){
-        int leaderA = find(a);
-        int leaderB = find(b);
-        if(leaderA != leaderB ){
-            parent[leaderA] = leaderB;
+        int leaderA = findLeader(a);
+        int leaderB = findLeader(b);
+        if(leaderA != leaderB){
+            if(size[leaderA] < size[leaderB]){
+                parent[leaderA] =  leaderB;
+                size[leaderB] += size[leaderA];
+            }
+            else{
+                parent[leaderB] = leaderA;
+                size[leaderA] += size[leaderB];
+            }
         }
         else{
-            ans[0] =a;
+            ans[0] = a;
             ans[1] = b;
             return;
         }
     }
 
-    public int find(int a){
+    public int findLeader(int a){
         if(parent[a] == a) return a;
 
-        return find(parent[a]);
+        return parent[a] = findLeader(parent[a]);
     }
 }
