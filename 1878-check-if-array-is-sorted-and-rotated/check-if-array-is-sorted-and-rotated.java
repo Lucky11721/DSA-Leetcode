1d0a9e3e1082a79dd5edcff6059927776fa6a19a
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-12-04 12:52:43 +0530

    Time: 2 ms (4.66%) | Memory: 42.9 MB (39.87%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,36 +1,18 @@
 class Solution {
     public boolean check(int[] arr) {
         int n = arr.length;
-        boolean sortedCheck = true;
+        int rotate = 0;
 
-        for(int i = 0 ; i < n -1 ; i++){
-            if(arr[i] > arr[i+1]) sortedCheck = false;
-        }
-        if(sortedCheck == true) return true;
-        for(int i = 1 ; i < n ; i++){
-            if(arr[i-1] > arr[i]){
-                reverse(arr , 0 , i-1);
-                reverse(arr , i , n-1);
-                break;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-        reverse(arr , 0 , n-1);
-        for(int i = 0 ; i < n -1 ; i++){
+        for(int i = 0 ; i < n-1  ; i++){
             if(arr[i] > arr[i+1]){
-                return false;
+                rotate++;
             }
         }
 
-        return true;
-    }
-    public void reverse(int[] arr , int start , int end){
-        while(start <= end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
+        System.out.println(rotate);
+        if(arr[n-1] > arr[0]) rotate++;
+        if(rotate <= 1) return true;
+
+        return false;
     }
 }
