1b3dfccc224eaef52bca0285c0d0347347e08473
Lucky <112880858+Lucky11721@users.noreply.github.com>
2025-12-24 15:52:54 +0530

    Time: 4 ms (82.79%) | Memory: 44.7 MB (13.65%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,35 +1,21 @@
 class Solution {
-    int[][] dp;
     public int minimumBoxes(int[] apple, int[] capacity) {
-        int totalPacks = 0;
+        int totalCap = 0;
+        for(int pack : apple) totalCap += pack;
 
-        for(int pack : apple){
-            totalPacks += pack;
+        Arrays.sort(capacity);
+        int n = capacity.length;
+        
+        int count = 0;
+
+        int currCapacity = 0;
+        for(int i = n-1 ; i>= 0 ; i--){
+            currCapacity += capacity[i];
+            count++;
+            if(currCapacity >= totalCap) break;
+            
         }
-        int totalCapacity = 0;
-        for(int cap : capacity){
-            totalCapacity += cap;
-        }
-        dp = new int[capacity.length][totalCapacity+1];
 
-        for(int[] arr : dp){
-            Arrays.fill(arr , -1);
-        }
-       
-
-        return helper(capacity , 0 , 0 , totalPacks);
-    }
-
-
-    public int helper(int[] arr , int index  , int curr_capacity , int limit){
-       if (curr_capacity >= limit) return 0;
-
-        if (index >= arr.length) return (int)1e9;
-
-        if(dp[index][curr_capacity] != -1) return dp[index][curr_capacity];
-        int pick = 1 + helper(arr , index+1 , curr_capacity + arr[index] , limit);
-        int skip = helper(arr , index+1 , curr_capacity , limit);
-
-        return dp[index][curr_capacity] = Math.min(pick , skip);
+        return count;
     }
 }
