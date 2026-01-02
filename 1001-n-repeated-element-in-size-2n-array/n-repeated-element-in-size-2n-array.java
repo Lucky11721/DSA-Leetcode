db59bd1c3bb08c891bb83fa5adc71dd4ea0b4665
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-01-02 15:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (6.11%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -3,17 +3,24 @@ class Solution {
         int n = nums.length;
         int k = n/2;
 
-       HashSet<Integer> set = new HashSet<>();
         for(int i = 0 ; i < n ; i++){
-            int ele = nums[i];
+            int curr = nums[i];
+            int next = -1;
+             if(i+1 < n){
+                next = nums[i+1];
+             }
+             int nextOfnext = -1;
+             if(i+2 < n){
+                nextOfnext = nums[i+2];
+             }
+             int next3 = -1;
+             if(i+3 < n){
+                next3 = nums[i+3];
+             }
 
-            if(set.contains(ele)){
-                return ele;
-            }
-
-            set.add(ele);
+           
+            if(curr == next || (curr == nextOfnext) || curr == next3) return curr;
         }
-
 
         return -1;
     }
