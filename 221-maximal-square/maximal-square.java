2491d9e05c9d3e61dd86dfca21737cbfd88cfc80
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-01-11 23:30:26 +0530

    Time: 7 ms (94.33%) | Memory: 71 MB (41.45%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,35 +1,20 @@
 class Solution {
-    int ans = 0;
-    int[][] dp;
     public int maximalSquare(char[][] matrix) {
-       
-         int m = matrix.length;
-         int n = matrix[0].length;
-         dp = new int[m+1][n+1];
-         for(int[] arr : dp){
-            Arrays.fill(arr ,-1);
-         }
-           helper(matrix , 0 , 0);
-         return ans * ans;
-    }
+        int ans = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for(int i =  m -1;i >= 0  ; i-- ){
+            for(int j = n-1  ;j >= 0;  j-- ){
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + Math.min(dp[i+1][j] ,
+                     Math.min(dp[i+1][j+1] , dp[i][j+1]));
 
-    public int helper(char[][] mat , int i , int j){
-        if(i >= mat.length || j >= mat[0].length) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int right = helper(mat , i , j+1);
-        int diag = helper(mat , i+1 , j+1);
-        int down = helper(mat , i+1 , j);
-
-        if(mat[i][j] == '1'){
-            ans = Math.max(ans ,dp[i][j] = 1+ Math.min(right , Math.min(diag , down)));
-
-            return  1+ Math.min(right , Math.min(diag , down));
-        }
-        else{
-            dp[i][j] = 0;
+                     ans = Math.max(ans , dp[i][j]);
+                }
+            }
         }
 
-        return dp[i][j];
+        return ans * ans;
     }
-
 }
