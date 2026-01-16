d5d37a1b7ef673fe190c5d2d7b79d07b4f715a5d
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-01-17 02:54:02 +0530

    Time: 5 ms (33.12%) | Memory: 54.9 MB (5.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,193 +1,39 @@
-class Pair {
-    int row;
-    int col;
-
-    Pair(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
+    public int numIslands(char[][] grid) {
+        
+        int m =  grid.length;
+        int n = grid[0].length;
 
-    public int numIslands(char[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == '1' && visited[i][j] == false) {
-                    dfs(arr, i, j, visited, n, m);
-                    ans++;
+        boolean visited[][] = new boolean[m][n];
+        int count =0;
+        for(int i = 0 ; i <  m ; i++){
+            for(int j = 0 ; j< n ; j++){
+                if(visited[i][j] == false && grid[i][j] == '1'){
+                    dfs(grid , i , j , visited);
+                    count++;
                 }
             }
         }
 
-        return ans;
+        return count;
     }
 
-    public void dfs(char[][] arr , int row, int col ,boolean[][] visited ,  int n , int m ){
-      
-        visited[row][col] =true;
-        if(row -1 >= 0 && arr[row-1][col] == '1' && visited[row-1][col] == false){
-            dfs(arr , row-1 , col , visited , n , m);
-        }
-         if(row +1 < n&& arr[row +1][col] == '1' && visited[row+1][col] == false){
-            dfs(arr , row +1 , col , visited , n , m);
-        }
-         if(col -1 >= 0 && arr[row][col -1] =='1'&& visited[row][col-1] == false){
-            dfs(arr , row , col-1 , visited , n , m);
-        }
-         if( col+1 < m &&  arr[row][col+1] == '1'&& visited[row][col+1] == false){
-            dfs(arr , row , col+1 , visited , n , m);
-        }
-    }
+    public void dfs(char[][] grid , int row , int col , boolean[][] visited){
 
-    public void helper(char[][] arr, int rows, int cols, boolean[][] visited, int n, int m) {
-        Queue<Pair> que = new LinkedList<>();
-        Pair p = new Pair(rows, cols);
-        que.add(p);
-        visited[rows][cols] = true;
+        visited[row][col] = true;
 
-        while (!que.isEmpty()) {
-            Pair index = que.remove();
-            int row = index.row;
-            int col = index.col;
+        int[] dx = {1 , 0 , -1 , 0};
 
-            // Top-left corner (0,0)
-            if (row == 0 && col == 0) {
-                if (row + 1 < n && arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-                if (col + 1 < m && arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-            }
+        int[] dy = {0 , 1 , 0 , -1};
 
-            // Top-right corner (0, m-1)
-            else if (row == 0 && col == m - 1) {
-                if (row + 1 < n && arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-                if (col - 1 >= 0 && arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-            }
+        for(int i = 0 ; i < 4 ; i++){
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
 
-            // Bottom-left corner (n-1, 0)
-            else if (row == n - 1 && col == 0) {
-                if (row - 1 >= 0 && arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-                if (col + 1 < m && arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-            }
-
-            // Bottom-right corner (n-1, m-1)
-            else if (row == n - 1 && col == m - 1) {
-                if (row - 1 >= 0 && arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-                if (col - 1 >= 0 && arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-            }
-
-            // Top edge (excluding corners)
-            else if (row == 0 && col > 0 && col < m - 1) {
-                if (col - 1 >= 0 && arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-                if (col + 1 < m && arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-                if (row + 1 < n && arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-            }
-
-            // Bottom edge (excluding corners)
-            else if (row == n - 1 && col > 0 && col < m - 1) {
-                if (col - 1 >= 0 && arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-                if (col + 1 < m && arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-                if (row - 1 >= 0 && arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-            }
-
-            // Left edge (excluding corners)
-            else if (col == 0 && row > 0 && row < n - 1) {
-                if (row - 1 >= 0 && arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-                if (row + 1 < n && arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-                if (col + 1 < m && arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-            }
-
-            // Right edge (excluding corners)
-            else if (col == m - 1 && row > 0 && row < n - 1) {
-                if (row - 1 >= 0 && arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-                if (row + 1 < n && arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-                if (col - 1 >= 0 && arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-            }
-
-            // Inner cells (not on edge or corner)
-            if (row > 0 && col > 0 && row < n - 1 && col < m - 1) {
-                if (arr[row - 1][col] == '1' && !visited[row - 1][col]) {
-                    que.add(new Pair(row - 1, col));
-                    visited[row - 1][col] = true;
-                }
-                if (arr[row][col - 1] == '1' && !visited[row][col - 1]) {
-                    que.add(new Pair(row, col - 1));
-                    visited[row][col - 1] = true;
-                }
-                if (arr[row + 1][col] == '1' && !visited[row + 1][col]) {
-                    que.add(new Pair(row + 1, col));
-                    visited[row + 1][col] = true;
-                }
-                if (arr[row][col + 1] == '1' && !visited[row][col + 1]) {
-                    que.add(new Pair(row, col + 1));
-                    visited[row][col + 1] = true;
-                }
-            }
+            if(newRow >= 0 && newRow < grid.length  && newCol >= 0 && newCol <
+             grid[0].length && grid[newRow][newCol] == '1' && visited[newRow][newCol] == false){
+                dfs(grid , newRow , newCol , visited);
+             }
         }
     }
 }
