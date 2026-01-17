108ed6773f528382ee0bf0880db05e580da8b6ec
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-01-18 04:13:33 +0530

    Time: 1 ms (99.65%) | Memory: 47 MB (34.52%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,27 +9,53 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> list = new ArrayList<>();
-        for(int i =0 ; i < lists.length ; i++){
-            while(lists[i] != null){
-                list.add(lists[i].val);
-                lists[i] = lists[i].next;
+   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1= list1.next;
             }
-        }
-
-        Collections.sort(list);
-
-        ListNode ans = new ListNode(-1);
-
-        ListNode temp = ans;
-
-        for(int num : list){
-            temp.next = new ListNode(num);
+            else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
             temp = temp.next;
         }
 
-        return ans.next;
+        if(list1 == null){
+            temp.next = list2;
+        }
+
+
+        if(list2 == null){
+            temp.next = list1;
+        }
+
+    return dummy.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+
+        return partitionAndmerge(0 , n-1 , lists);
+    }
+
+    public ListNode partitionAndmerge(int start , int end ,ListNode[] lists ){
+        if(start > end) return null;
+        if(start == end) return lists[start];
+
+        int mid = (start + end)/2;
+
+        ListNode L1 = partitionAndmerge(start , mid , lists);
+        ListNode L2  = partitionAndmerge(mid+1 , end , lists);
+
+
+        return mergeTwoLists(L1 , L2);
+
+
 
     }
 }
