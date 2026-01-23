3f8932ca680edebd1dd04da577435d9269eeef02
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-01-23 19:51:42 +0530

    Time: 862 ms (6.16%) | Memory: 51.3 MB (46.32%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -4,17 +4,15 @@ class Solution {
 
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            if(arr[i] < k) ans++;
-            int product = arr[i];
-            for (int j = i +1 ; j < n; j++) {
-                product *= arr[j];
-                if (product < k) {
-                    ans++;
-                }
-                else if(product >= k) break;
-            }
+       for(int i = 0 ;  i< n ; i++){
+        if(arr[i] >= k ) continue;
+        int product  =1;
+        for(int j = i ; j < n ; j++){
+            product = product*arr[j];
+            if(product < k) ans++;
+            else if(product > k) break;
         }
+       }
         return ans;
     }
 }
