67e8e183dbf7fa12d5cea2b29222ec869f2812fc
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-02-12 11:26:54 +0530

    Time: 10 ms (20.89%) | Memory: 51.4 MB (5.04%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,15 +1,35 @@
 class NumArray {
-    int[] arr;
+    int[] segmentTree;
+    int n;
     public NumArray(int[] nums) {
-        arr = nums;
+         n  = nums.length;
+        segmentTree = new int[4*n];
+        buildTree(nums , 0 , 0 , n-1);
     }
     
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i = left ; i <=right ; i++){
-            sum = sum + arr[i];
+        return query(left , right , 0 , 0 , n-1);
+    }
+
+    public int query(int start , int end , int idx , int i , int j){
+        if(i > end || j < start) return 0;
+        if(i >= start && j <= end){
+            return segmentTree[idx];
         }
-        return sum;
+        int mid = i + (j-i)/2;
+        return query(start , end , 2*idx+1 , i , mid) + query(start , end , 2*idx+2 , mid+1 , j);
+    }
+
+
+    public void buildTree(int[] nums , int idx , int left , int right){
+        if(left == right){
+            segmentTree[idx] = nums[left];
+            return;
+        }
+        int mid = left + (right - left)/2;
+        buildTree(nums , 2 *idx +1 , left , mid);
+        buildTree(nums , 2  * idx +2 , mid + 1 , right);
+        segmentTree[idx] = segmentTree[2*idx+1] + segmentTree[2*idx+2];
     }
 }
 
