a10c6d98890c33ca5c53ca49a4b3e5f162f3d62e
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-02-16 22:48:11 +0530

    Time: 1 ms (60.34%) | Memory: 42.5 MB (71.14%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,26 +1,14 @@
 class Solution {
     public int reverseBits(int n) {
-        StringBuilder sb = new StringBuilder();
-
-        // Step 1: extract exactly 32 bits
-        for (int i = 0; i < 32; i++) {
-            int bit = n & 1;
-            sb.append(bit);      // appending 0 or 1
-            n = n >>> 1;         // logical right shift
-        }
-
-        // Step 2: reverse bits
-        sb.reverse();
-
-        System.out.println(sb);
-        // Step 3: convert binary string to integer
+        if(n == 0) return 0;
         int ans = 0;
 
-       int power = 0;
-
-       for(int i = 0 ;i < 32 ; i++){
-        ans += (sb.charAt(i) - '0') * Math.pow(2,i);
-       }
+        for(int i = 0 ; i < 32 ; i++ ){
+            ans = ans<<1;
+            int setBit = n &1;
+            ans = ans|setBit;
+            n = n >> 1;
+        }
 
         return ans;
     }
