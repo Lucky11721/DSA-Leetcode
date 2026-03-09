e4f3b2096f60b0258243570d8da2bc02eb0646e2
Lucky <112880858+Lucky11721@users.noreply.github.com>
2026-03-09 23:35:50 +0530

    Time: 149 ms (78.95%) | Memory: 47 MB (85.53%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -1,51 +1,29 @@
 class Solution {
 
     static final int MOD = 1000000007;
-    int[][][] dp;
-    int limit;
 
     public int numberOfStableArrays(int zero, int one, int limit) {
 
-        this.limit = limit;
+        int[][][] dp = new int[zero + 1][one + 1][2];
 
-        dp = new int[zero + 1][one + 1][3];
+        dp[0][0][0] = 1;
+        dp[0][0][1] = 1;
 
-        for(int i = 0; i <= zero; i++){
-            for(int j = 0; j <= one; j++){
-                for(int k = 0; k < 3; k++){
-                    dp[i][j][k] = -1;
+        for (int z = 0; z <= zero; z++) {
+            for (int o = 0; o <= one; o++) {
+
+                // ending with 0
+                for (int k = 1; k <= limit && k <= z; k++) {
+                    dp[z][o][0] = (dp[z][o][0] + dp[z - k][o][1]) % MOD;
+                }
+
+                // ending with 1
+                for (int k = 1; k <= limit && k <= o; k++) {
+                    dp[z][o][1] = (dp[z][o][1] + dp[z][o - k][0]) % MOD;
                 }
             }
         }
 
-        return dfs(zero, one, 2);
-    }
-
-    int dfs(int zero, int one, int last){
-
-        if(zero == 0 && one == 0) return 1;
-
-        if(dp[zero][one][last] != -1)
-            return dp[zero][one][last];
-
-        long ans = 0;
-
-        // place zeros
-        if(last != 0){
-            for(int k = 1; k <= limit && k <= zero; k++){
-                ans += dfs(zero - k, one, 0);
-                ans %= MOD;
-            }
-        }
-
-        // place ones
-        if(last != 1){
-            for(int k = 1; k <= limit && k <= one; k++){
-                ans += dfs(zero, one - k, 1);
-                ans %= MOD;
-            }
-        }
-
-        return dp[zero][one][last] = (int) ans;
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 }
